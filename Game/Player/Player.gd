3d86bed8c9fdab16b6c4a6192a0b0593d8efad25
Ryan <artlessavian@gmail.com>
2023-08-26 04:04:00 -0700

    Extend parent class instead.

--- Game/Player/Player.gd
+++ Game/Player/Player.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+extends PhysicsBody2D
 class_name Player
 
 const SPEED = 300.0
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = -400.0
 const GRAVITY = 980
 const MAX_FLOOR_ANGLE = 45
 
+var velocity: Vector2 = Vector2.ZERO
 var _state = GroundedState.new()
 var state_time = 0
 var ground_normal = Vector2.UP;
@@ -16,7 +17,7 @@ func _physics_process(delta):
 	_state.physics_process(self, delta)
 	state_time += 1
 	
-	bingus(velocity * delta)
+	move_and_slide(velocity * delta)
 	#var collision: KinematicCollision2D = move_and_collide(velocity * delta)
 	#if collision != null:
 	#	if velocity.y >= 0 and collision.get_local_shape() == $GroundCheck:
@@ -42,7 +43,7 @@ func _physics_process(delta):
 	arrow.scale = Vector2.ONE * max(1, sqrt(velocity.length()))
 	arrow.target_position = velocity / arrow.scale
 
-func bingus(vec):
+func move_and_slide(vec):
 	$LeftLow.disabled = vec.x > 0
 	$LeftHigh.disabled = vec.x > 0
 	$RightLow.disabled = vec.x < 0
@@ -57,11 +58,11 @@ func bingus(vec):
 		_state.on_touch_ground(self, collision)
 		return
 	
-	if collision.get_local_shape in [$LeftLow, $LeftHigh, $RightLow, $RightHigh]:
+	if collision.get_local_shape() in [$LeftLow, $LeftHigh, $RightLow, $RightHigh]:
 		velocity.x = 0
 		print(collision.get_depth())
 	
-	if collision.get_local_shape in [$Head]:
+	if collision.get_local_shape() in [$Head]:
 		velocity.y = 0
 		print(collision.get_depth())
 
